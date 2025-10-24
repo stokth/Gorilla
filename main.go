7d0eb19f668ca12b042c6e09d3cd9046a929028c
Sergey <stokth.geek@gmail.com>
2025-10-25 00:54:56 +0500

    Реализованы методы GET and POST

--- main.go
+++ main.go
@@ -20,7 +20,17 @@ type TaskRequest struct {
 
 func getTask(c echo.Context) error {
 	return c.JSON(http.StatusOK, tasks)
+}
 
+func postTask(c echo.Context) error {
+	var req TaskRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+
+	tasks = append(tasks, Task{ID: len(tasks) + 1, Task: req.Task})
+	return c.JSON(http.StatusCreated, tasks)
 }
 
 func main() {
@@ -30,6 +40,7 @@ func main() {
 	c.Use(middleware.Logger())
 
 	c.GET("/tasks", getTask)
+	c.POST("/tasks", postTask)
 
 	c.Start("localhost:8080")
 }
