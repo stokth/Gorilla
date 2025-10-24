338f25a585b4a81663c6ca9d60a867d88529f9e6
Sergey <stokth.geek@gmail.com>
2025-10-25 00:39:20 +0500

    first get handle

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Task struct {
+	ID   int    `json:"id"`
+	Task string `json:"task"`
+}
+
+var tasks = []Task{}
+
+type TaskRequest struct {
+	Task string `json:"task"`
+}
+
+func getTask(c echo.Context) error {
+	return c.JSON(http.StatusOK, tasks)
+
+}
 
 func main() {
-	fmt.Println("Hello World")
+	c := echo.New()
+
+	c.Use(middleware.CORS())
+	c.Use(middleware.Logger())
+
+	c.GET("/tasks", getTask)
+
+	c.Start("localhost:8080")
 }
